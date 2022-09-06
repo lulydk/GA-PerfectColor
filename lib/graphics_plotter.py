4ecc45c1754a661fadabb1bac63e9b6b4657c61a
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2022-09-05 23:42:58 -0300

    Plotter implemented

--- lib/graphics_plotter.py
+++ lib/graphics_plotter.py
@@ -1,1 +1,40 @@
-# todo
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator)
+
+def get_values():
+    path = "C:/Users/lulyd/Documents/GA-PerfectColor/output/graphics.txt"
+    with open(path, "r") as file:
+        population_size = int(file.readline())
+        content = file.read()
+    array = list(map(float, content.split()))
+    return population_size, array
+
+def main():
+    population_size, array = get_values()
+    fig, ax = plt.subplots()
+    ax.set_title('Fitness through the generations')
+    ax.set_ylabel('Fitness')
+    ax.set_xlabel('Colors through generations')
+    ax.xaxis.set_minor_locator(MultipleLocator(population_size))
+    x = list(map(lambda num: num/population_size,range(len(array))))
+    #ax.set_xlabel('Generations')
+    #ax.xaxis.set_minor_locator(MultipleLocator(1))
+    #ax.plot(x, array)
+    ax.plot(array)
+    plt.show()
+
+def mainy():
+    #fitness = []
+    path = "C:/Users/lulyd/Documents/GA-PerfectColor/output/graphics.txt"
+    with open(path, "r") as file:
+        population_size = file.readline()
+        content = file.read()
+    array = content.split()
+    numbers = list(map(float, array))
+    plt.plot(numbers)
+    plt.ylabel('Fitness')
+    plt.xlabel('Colors through generations')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -11,33 +11,38 @@ from colormath.color_objects import LabColor, sRGBColor
 from genetic_algorithm.artist_palette import ArtistPalette
 
 
-def cut(iteration, delta, best, cut_method, cut_generation, cut_delta):
+def cut(iteration, delta, best_ever, cut_method, cut_generation, cut_delta):
     if (cut_method == GENERATIONS):
         return iteration >= cut_generation
     elif (cut_method == TRESHOLD):
         return delta <= cut_delta
     else:
-        if (iteration - best[0] > cut_generation and abs(delta - best[1]) < ERROR):
+        if (iteration - best_ever[0] > cut_generation and abs(delta - best_ever[1]) < ERROR):
             print("Cut by exhaustion")
             return True
 
 def run_simulation(target, input_handler):
     artist_palette = ArtistPalette(input_handler.color_palette, input_handler.population_n, target)
-    best = artist_palette.best_color
-    delta_e = best.get_fitness(target)
+    all_best = []
+    all_best.append(artist_palette.best_color)
+    delta_e = all_best[0].get_delta(target)
+    all_colors = []
+    all_colors.append(artist_palette.color_palette)
     iteration_count = 0
     best_ever = np.zeros(2)
     best_ever[0], best_ever[1] = iteration_count, delta_e
     while (not cut(iteration_count,delta_e,best_ever,input_handler.cut_method,input_handler.cut_generation,input_handler.cut_delta)):
         artist_palette.mix_new_generation(input_handler)
-        best = artist_palette.best_color
-        delta_e = best.get_delta(target)
+        all_colors.append(artist_palette.color_palette)
+        curr_best = artist_palette.best_color
+        delta_e = curr_best.get_delta(target)
+        all_best.append(curr_best)
         if (abs(delta_e - best_ever[1]) >= ERROR):
             best_ever[0] = iteration_count
             best_ever[1] = delta_e
         print(f"#{iteration_count} with delta_e = {delta_e}")
         iteration_count += 1
-    return best
+    return all_colors, all_best, iteration_count
 
 def run_visualization(target, finished_color, input_handler):
     app = QApplication(sys.argv)
@@ -78,21 +83,27 @@ def write_recipe(is_rgb, target, best):
         print(add_text, file=external_file)
     print("Results in output/recipe.txt")
 
+def write_output_data(all_best_colors, target, population_size):
+    with open("output/graphics.txt", "w") as external_file:
+        print(population_size, file=external_file)
+        for generation in all_best_colors:
+            generation_string = ' '.join([f'{color.get_fitness(target)}' for color in generation])
+            print(generation_string, file=external_file)
+    print("Simulation data in output/graphics.txt")
+
 def main():
     with open('config.json', 'r') as f:
         input = json.load(f)
         input_handler = InputHandler(input)
     # Simulation
     target = input_handler.target_color
-    finished_color = run_simulation(target, input_handler)
+    all_colors, all_best_colors, total_iterations = run_simulation(target, input_handler)
+    finished_color = all_best_colors[total_iterations]
     # File outputs
     ## Recipe with color proportions
     write_recipe(input_handler.work_with_rgb, target, finished_color)
     ## Data for graphics
-    with open("output/graphics.txt", "w") as external_file:
-        add_text = f""
-        print(add_text, file=external_file)
-    print("Simulation data in output/graphics.txt")
+    write_output_data(all_colors, target, input_handler.population_n)
     # Visualization
     run_visualization(target, finished_color, input_handler)
 
